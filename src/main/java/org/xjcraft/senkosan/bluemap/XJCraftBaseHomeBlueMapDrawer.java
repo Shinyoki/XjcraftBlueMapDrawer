32719ef9956d11ea8816cb2e07e96b09b95c6182
WaterLike <56620507+Shinyoki@users.noreply.github.com>
2022-08-14 18:58:07 +0800

    优化： - 使用自己的线程池来处理非Spigot相关任务

--- src/main/java/org/xjcraft/senkosan/bluemap/XJCraftBaseHomeBlueMapDrawer.java
+++ src/main/java/org/xjcraft/senkosan/bluemap/XJCraftBaseHomeBlueMapDrawer.java
@@ -9,15 +9,43 @@ import org.xjcraft.senkosan.bluemap.exception.XBMPluginException;
 import org.xjcraft.senkosan.bluemap.manager.XJCraftBlueMapContext;
 import org.xjcraft.senkosan.bluemap.utils.Log;
 
+import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public final class XJCraftBaseHomeBlueMapDrawer extends JavaPlugin {
 
     private static XJCraftBaseHomeBlueMapDrawer instance;   // 插件
     private static XBMCommandHandler commandHandler;        // 指令处理器
+    private static ExecutorService executorService;         // 线程池
 
     public static XJCraftBaseHomeBlueMapDrawer getInstance() {
         return instance;
+    }
+
+    @Override
+    public void onLoad() {
+        initThreadPool();
+    }
+
+    @Override
+    public void onDisable() {
+
+        if (Objects.nonNull(executorService)) {
+            Log.info("关闭线程池中...");
+            executorService.shutdown();
+
+            if (executorService.isShutdown()) {
+                Log.info("线程池已关闭！");
+
+            } else {
+                Log.info("线程池未能关闭！");
+                executorService.shutdownNow();
+
+            }
+        }
+
     }
 
     @Override
@@ -41,11 +69,19 @@ public final class XJCraftBaseHomeBlueMapDrawer extends JavaPlugin {
         try {
             // 需回到插件主线程
             Bukkit.getScheduler().runTask(this, () -> {
+                // 重载配置文件
                 this.saveDefaultConfig();
                 this.reloadConfig();
+
+                // 判断插件是否已经加载好
                 checkInit();
+
                 Log.info("绘制Markers中...");
+
+                // 渲染所有玩家的Markers
                 XJCraftBlueMapContext.getBlueMapManager().renderAll();
+
+                // 注册指令执行器
                 Optional.ofNullable(getCommand("xjb"))
                         .ifPresent(command -> {
                             commandHandler = new XBMCommandHandler();
@@ -53,18 +89,21 @@ public final class XJCraftBaseHomeBlueMapDrawer extends JavaPlugin {
                         });
                 Log.info("初始化完成");
             });
-        } catch (Exception ignore) {}
+        } catch (Exception ignore) {
+        }
 
     }
 
     private void checkInit() {
 
         PluginManager pluginManager = Bukkit.getPluginManager();
+
         Optional.ofNullable(pluginManager.getPlugin("BlueMap"))
                 .orElseThrow(() -> {
                     Log.error("未能正确获取BlueMap插件，请检查是否正确加载了BlueMap插件");
                     return new XBMPluginException("未能正确获取BlueMap插件，请检查是否正确加载了BlueMap插件");
                 });
+
         Optional.ofNullable(pluginManager.getPlugin("XJCraftAudit"))
                 .orElseThrow(() -> {
                     Log.error("未能正确获取XJCraftAudit插件，请检查是否正确加载了XJCraftAudit插件");
@@ -75,5 +114,23 @@ public final class XJCraftBaseHomeBlueMapDrawer extends JavaPlugin {
 
     public static XBMCommandHandler getCommandHandler() {
         return commandHandler;
+    }
+
+    private static synchronized ExecutorService initThreadPool() {
+
+        if (Objects.isNull(executorService)) {
+            // 初始化线程池
+            executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors(), r -> {
+                Thread thread = new Thread(r);
+                thread.setName("XJCraftBaseHomeBlueMapDrawer-Thread");
+                return thread;
+            });
+        }
+        return executorService;
+
+    }
+
+    public static synchronized ExecutorService getExecutorService() {
+        return executorService == null ? initThreadPool() : executorService;
     }
 }
--- src/main/java/org/xjcraft/senkosan/bluemap/manager/DefaultBlueMapManager.java
+++ src/main/java/org/xjcraft/senkosan/bluemap/manager/DefaultBlueMapManager.java
@@ -63,8 +63,8 @@ public class DefaultBlueMapManager extends ConfigurableBlueMapManager {
         for (MarkerType type : MarkerType.values()) {
             getMarkerAPI().getMarkerSet(type.getMarkerSetId())
                     .ifPresent(markerSet -> {
-                count.addAndGet(markerSet.getMarkers().size());
-            });
+                        count.addAndGet(markerSet.getMarkers().size());
+                    });
         }
         return count.get();
     }
@@ -73,55 +73,53 @@ public class DefaultBlueMapManager extends ConfigurableBlueMapManager {
 
         AtomicLong allBegin = new AtomicLong(System.currentTimeMillis());
         Log.info("需删除的Marker数量: " + getMarkerCount());
-        Log.info("开始渲染所有玩家的地图，当前时刻：" + System.currentTimeMillis());
-        // 切异步：删除所有Marker
-        Bukkit.getScheduler().runTaskAsynchronously(XJCraftBaseHomeBlueMapDrawer.getInstance(), () -> {
 
+        // 提前加载所有的离线玩家名称
+        List<String> playerNames = Arrays.stream(Bukkit.getOfflinePlayers())
+                .map(OfflinePlayer::getName).collect(Collectors.toList());
+        Log.info("离线玩家数量*2：" + playerNames.size() * 2);
+
+        // 切异步：删除所有Marker
+        XJCraftBaseHomeBlueMapDrawer.getExecutorService().submit(() -> {
+            Log.info("开始渲染所有玩家的地图，当前时刻：" + System.currentTimeMillis());
             AtomicLong beforeDeleteMillis = new AtomicLong(System.currentTimeMillis());
+
             try {
+
                 // 删除原有的所有标记
                 Arrays.stream(MarkerType.values())
                         .forEach(markerType -> {
                             getMarkerAPI().removeMarkerSet(markerType.getMarkerSetId());
                         });
+                // BlueMap IO操作
                 getMarkerAPI().save();
                 Log.info("删除线程：" + Thread.currentThread().getName() + "，耗时：" + (System.currentTimeMillis() - beforeDeleteMillis.get()) + "ms");
+
+                AtomicLong beforeRender = new AtomicLong(System.currentTimeMillis());
+                // 渲染所有标记
+                playerNames.forEach(this::renderMarker);
+                Log.info("渲染线程：" + Thread.currentThread().getName() + "耗时: " + (System.currentTimeMillis() - beforeRender.get()) + "ms");
+
+                AtomicLong beforeSaveMillis = new AtomicLong(System.currentTimeMillis());
+                try {
+                    // BlueMap IO写入文件
+                    getMarkerAPI().save();
+                    Log.info("保存时的线程：" + Thread.currentThread().getName() + "耗时: " + (System.currentTimeMillis() - beforeSaveMillis.get()) + "ms");
+                    Log.info("渲染所有玩家的地图完成，当前时刻：" + System.currentTimeMillis() + "，总耗时：" + (System.currentTimeMillis() - allBegin.get()) + "ms");
+
+                } catch (IOException e) {
+                    Log.error("保存Marker失败");
+                    throw new RuntimeException(e);
+                }
+
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
 
-            // 切Main线程：获取所有离线玩家的名称
-            Bukkit.getScheduler().runTask(XJCraftBaseHomeBlueMapDrawer.getInstance(), () -> {
-
-                List<String> playerNames = Arrays.stream(Bukkit.getOfflinePlayers())
-                        .map(OfflinePlayer::getName).collect(Collectors.toList());
-
-                Log.info("离线玩家数量*2：" + playerNames.size() * 2);
-
-                // 切异步：渲染所有离线玩家的地图
-                Bukkit.getScheduler().runTaskAsynchronously(XJCraftBaseHomeBlueMapDrawer.getInstance(), () -> {
-
-                    AtomicLong beforeRender = new AtomicLong(System.currentTimeMillis());
-                    // 渲染所有标记
-                    playerNames.forEach(this::renderMarker);
-                    Log.info("渲染线程：" + Thread.currentThread().getName() + "耗时: " + (System.currentTimeMillis() - beforeRender.get()) + "ms");
-                    AtomicLong beforeSaveMillis = new AtomicLong(System.currentTimeMillis());
-                    try {
-                        getMarkerAPI().save();
-                        Log.info("保存时的线程：" + Thread.currentThread().getName() + "耗时: " + (System.currentTimeMillis() - beforeSaveMillis.get()) + "ms");
-                        Log.info("渲染所有玩家的地图完成，当前时刻：" + System.currentTimeMillis() + "，总耗时：" + (System.currentTimeMillis() - allBegin.get()) + "ms");
-                    } catch (IOException e) {
-                        Log.error("保存Marker失败");
-                        throw new RuntimeException(e);
-                    }
-
-                });
-
-            });
-
         });
 
     }
+
 
     public void changeMarkerCreator(MarkerCreator creator) {
         this.markerCreator = creator;
